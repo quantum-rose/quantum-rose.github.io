735694883f8cd40f8de333c36964b8c825ee2be9
崔豪杰 <cui-haojie@qq.com>
2025-04-17 11:03:44 +0800

    update cjjyg

--- cjjyg/index.c4f94.js
+++ cjjyg/index.c4f94.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.bd17b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
