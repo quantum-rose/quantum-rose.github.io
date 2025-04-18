34354285a3192cfc7588c3503a711df6dd6daba0
崔豪杰 <cui-haojie@qq.com>
2025-04-18 17:52:36 +0800

    update cjjyg

--- cjjyg/index.aaf93.js
+++ cjjyg/index.aaf93.js
@@ -1,4 +1,4 @@
-System.register(["./application.2f8db.js"], function (_export, _context) {
+System.register(["./application.776a1.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
